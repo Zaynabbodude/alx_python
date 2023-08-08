46698fe9230c045cb8037b19267810619926bb42
zaynabbodude <zainabbodude1@gmail.com>
2023-08-09 00:15:54 +0100

    my commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,15 @@
+# word = "Holberton"
+# word_first_3 = word[:3]
+# word_last_2 = word[7:]
+# middle_word = word[1:8]
+# print("First 3 letters: {}".format(word_first_3))
+# print("Last 2 letters: {}".format(word_last_2))
+# print("middle letters: {}".format(middle_word))
+# number = len(word)
+
 word = "Holberton"
-word_first_3 = word[0:3]
-word_last_2 = word[7:]
-middle_word = word[1:8]
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("middle letters: {}".format(middle_word))
-number = len(word)
+print("First 3 letters: {}".format(word[:3]))
+
+print("Last 2 letters: {}".format(word[7:]))
+
+print("Middle word: {}".format(word[1:8]))
