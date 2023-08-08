1ebf6f3947931a8505ce1d1e71c735bee6d3b0bd
zaynabbodude <zainabbodude1@gmail.com>
2023-08-09 00:25:48 +0100

    my commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,8 @@
 word = "Holberton"
-
+word2= "School"
 print("First 3 letters: {}".format(word[:3]))
-
+print("First 3 letters: {}".format(word2[:3]))
 print("Last 2 letters: {}".format(word[7:]))
-
+print("Last 2 letters: {}".format(word2[4:]))
 print("Middle word: {}".format(word[1:8]))
+print("Middle word: {}".format(word2[1:6]))
