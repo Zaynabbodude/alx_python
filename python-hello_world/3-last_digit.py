2c1aa33fbf28c9eae7b45bc025611e4af9d55fb6
zaynabbodude <zainabbodude1@gmail.com>
2023-08-10 21:43:02 +0100

    my commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,2 +1,13 @@
 import random
 number = random.randint(-10000, 10000)
+last_digit = 0
+
+if last_digit of number > 5
+    print("last_digit number > 5 == 0")
+else: 
+
+if last_digit number == 0
+print("last_digit number == 0")
+
+if last_digit number < 6 and not 0
+print("last_digit number")
