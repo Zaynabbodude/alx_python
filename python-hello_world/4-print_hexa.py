265892dcb49b6df25b679053ad20eee95bfb763c
zaynabbodude <zainabbodude1@gmail.com>
2023-08-21 18:33:39 +0100

    my commit

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,7 +1,7 @@
 for number in range (99):
     hex = 0
     hex_value = "{0:x}".format (number)
-    print("{} = {} * {}".format( number, hex_value, hex))
+    print("{} = {} x {}".format( number, hex_value, hex))
       
      
      
