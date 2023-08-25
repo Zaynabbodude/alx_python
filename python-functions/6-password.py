0c4e8582d6b17e0d039969b616fcd7fc1a14335a
zaynabbodude <zainabbodude1@gmail.com>
2023-08-26 00:01:16 +0100

    my commit

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,8 +1,5 @@
 def validate_password(password):
     return True
-if (password is Password123):
-else:
-    return False
 
 print(validate_password("Password123"))
 print(validate_password("abc123"))
