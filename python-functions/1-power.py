1db34603008df2de7f5507dbbb24b6cbb900d2c9
zaynabbodude <zainabbodude1@gmail.com>
2023-08-26 01:52:17 +0100

    my commit

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -2,12 +2,13 @@ def pow(a, b):
     return a**b
 
 print(2,2)
-pow(2,2)
 print(98,2)
-pow(98,2)
 print(98,0)
-pow(98,0)
 print(100,-2)
-pow(100,-2)
 print(-4,5)
+
+pow(2,2)
+pow(98,2)
+pow(98,0)
+pow(100,-2)
 pow(-4,5)
