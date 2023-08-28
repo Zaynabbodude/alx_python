19020a8c352264c2092f652fab7b3d5b276f6122
zaynabbodude <zainabbodude1@gmail.com>
2023-08-28 23:41:47 +0100

    my commit

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,7 +1,4 @@
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
 
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+
