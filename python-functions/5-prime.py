ccb37e4ca1277d619b4406b7a843a39392105c2e
zaynabbodude <zainabbodude1@gmail.com>
2023-08-29 00:08:01 +0100

    my commit

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,17 +1,7 @@
 def is_prime(number):
-
-  if number == 17:
-   result = True
-   print(result)
-  else:
-   result = False
-   return result
-
-print(17)
-is_prime(17)
-print(15)
-is_prime(15)
-print(-5)
-is_prime(-5)
-print(0)
-is_prime(0)
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+        return True  
