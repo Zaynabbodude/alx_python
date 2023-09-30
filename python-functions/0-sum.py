6054b1be143922f02e594f11dcb2d1724667f60a
zaynabbodude <zainabbodude1@gmail.com>
2023-09-30 09:54:55 +0100

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,3 +1,20 @@
-def add(a, b):
-    return a + b
+#def add(a, b):
+    #return a + b
 
+#Program to compute sum and average  of 2 numbers
+#Data input
+#A= float(input("enter data 1")) 
+#B= float(input("enter data 2"))
+#Data processing
+#sum= A + B
+#mean = sum/2
+#compute
+#print("sum=",sum,"\n mean=",mean)
+
+def sum(A, B):
+    return A + B
+A= float (50)
+B= float (20)
+sum= A + B
+mean= sum/2
+print("sum")
