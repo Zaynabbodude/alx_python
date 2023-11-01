ff3a2e25e89b7b3c9ef2b4c1d474afd7b2bdd487
zaynabbodude <zainabbodude1@gmail.com>
2023-11-01 12:42:17 +0100

    my commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -3,18 +3,18 @@
 
 #Program to compute sum and average  of 2 numbers
 #Data input
-#A= float(input("enter data 1")) 
-#B= float(input("enter data 2"))
+A= float(input("enter data 1")) 
+B= float(input("enter data 2"))
 #Data processing
-#sum= A + B
-#mean = sum/2
-#compute
-#print("sum=",sum,"\n mean=",mean)
-
-def sum(A, B):
-    return A + B
-A= float (50)
-B= float (20)
 sum= A + B
-mean= sum/2
-print("sum")
+mean = sum/2
+#compute
+print("sum=",sum,"\n mean=",mean)
+
+#def sum(A, B):
+ #   return A + B
+#A= float (50)
+#B= float (20)
+#sum= A + B
+#mean= sum/2
+#print("sum")
